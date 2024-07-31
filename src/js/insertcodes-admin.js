eef4515d5a462dfae6a397f4dc662fc915b5d610
kawsarahmedr <kawsar.ahmed198133@gmail.com>
2024-07-31 22:18:27 +0600

    Update admin script

--- src/js/insertcodes-admin.js
+++ src/js/insertcodes-admin.js
@@ -4,15 +4,16 @@
 		$.ready.then(function () {
 			var defaultSettings = wp.codeEditor.defaultSettings ? _.clone(wp.codeEditor.defaultSettings) : {};
 
-			// HTML Editor
+			// HTML Editor.
 			var htmlSettings = _.extend({}, defaultSettings, {
 				codemirror: _.extend({}, defaultSettings.codemirror, {
 					mode: 'htmlmixed'
 				})
 			});
-			 wp.codeEditor.initialize($('#insertcodes_header'), htmlSettings);
-			 wp.codeEditor.initialize($('#insertcodes_body'), htmlSettings);
-			 wp.codeEditor.initialize($('#insertcodes_footer'), htmlSettings);
+
+			wp.codeEditor.initialize($('#insertcodes_header'), htmlSettings);
+			wp.codeEditor.initialize($('#insertcodes_body'), htmlSettings);
+			wp.codeEditor.initialize($('#insertcodes_footer'), htmlSettings);
 		});
 	});
 })(jQuery);
