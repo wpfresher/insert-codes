e08c46ab92f98c505e65a0b9466aa05d1ade776a
kawsarahmedr <kawsar.ahmed198133@gmail.com>
2024-08-31 14:48:31 +0600

    Add php initializer in admin js

--- src/js/insertcodes-admin.js
+++ src/js/insertcodes-admin.js
@@ -10,10 +10,18 @@
 					mode: 'htmlmixed'
 				})
 			});
+			// PHP Editor.
+			var phpSettings = _.extend({}, defaultSettings, {
+				codemirror: _.extend({}, defaultSettings.codemirror, {
+					mode: 'application/x-httpd-php' // Or use "php" instead.
+				})
+			});
 
+			// Initialize the code editors.
 			wp.codeEditor.initialize($('#insertcodes_header'), htmlSettings);
 			wp.codeEditor.initialize($('#insertcodes_body'), htmlSettings);
 			wp.codeEditor.initialize($('#insertcodes_footer'), htmlSettings);
+			wp.codeEditor.initialize($('#insertcodes_php'), phpSettings);
 		});
 	});
 })(jQuery);
