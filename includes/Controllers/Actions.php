db4787e3a90d78af6ec760d5b91cf85fe3dbe2eb
kawsarahmedr <kawsar.ahmed198133@gmail.com>
2024-09-05 09:51:56 +0600

    Fix php code input field and sanitization

--- includes/Controllers/Actions.php
+++ includes/Controllers/Actions.php
@@ -17,6 +17,7 @@ class Actions {
 	 */
 	public function __construct() {
 		add_action( 'admin_post_insertcodes_hbf_scripts', array( __CLASS__, 'handle_hbf_scripts' ) );
+		add_action( 'admin_post_insertcodes_snippets', array( __CLASS__, 'handle_snippets' ) );
 		add_action( 'admin_post_insertcodes_settings', array( __CLASS__, 'handle_settings' ) );
 	}
 
@@ -39,6 +40,28 @@ class Actions {
 		update_option( 'insertcodes_footer', $footer_scripts );
 
 		insertcodes()->add_flash_notice( __( 'Codes saved successfully.', 'insert-codes' ) );
+		wp_safe_redirect( wp_get_referer() );
+		exit();
+	}
+
+	/**
+	 * Updating settings.
+	 *
+	 * @since 1.0.0
+	 * @return void
+	 */
+	public static function handle_snippets() {
+		check_admin_referer( 'insertcodes_snippets' );
+
+		$php_snippets = isset( $_POST['insertcodes_php'] ) ? sanitize_textarea_field( wp_unslash( $_POST['insertcodes_php'] ) ) : '';
+
+		// Decode html entity.
+		$php_snippets = html_entity_decode( $php_snippets, ENT_QUOTES, 'UTF-8' );
+
+		// Updating options.
+		update_option( 'insertcodes_php', $php_snippets );
+
+		insertcodes()->add_flash_notice( __( 'PHP code snippets saved successfully.', 'insert-codes' ) );
 		wp_safe_redirect( wp_get_referer() );
 		exit();
 	}
--- includes/functions.php
+++ includes/functions.php
@@ -77,3 +77,109 @@ function insertcodes_get_allowed_html() {
 		),
 	);
 }
+
+/**
+ * Get php allowed html.
+ *
+ * @since 1.0.0
+ * @return array
+ */
+function insertcodes_get_php_allowed_html() {
+	return array(
+		'pre'        => array(),
+		'code'       => array(),
+		'br'         => array(),
+		'strong'     => array(),
+		'em'         => array(),
+		'p'          => array(),
+		'div'        => array(),
+		'span'       => array(),
+		'img'        => array(),
+		'ul'         => array(),
+		'ol'         => array(),
+		'li'         => array(),
+		'a'          => array(),
+		'iframe'     => array(),
+		'blockquote' => array(),
+		'h1'         => array(),
+		'h2'         => array(),
+		'h3'         => array(),
+		'h4'         => array(),
+		'h5'         => array(),
+		'h6'         => array(),
+		'table'      => array(),
+		'thead'      => array(),
+		'tbody'      => array(),
+		'tfoot'      => array(),
+		'tr'         => array(),
+		'th'         => array(),
+		'td'         => array(),
+		'hr'         => array(),
+		'form'       => array(),
+		'input'      => array(),
+		'select'     => array(),
+		'option'     => array(),
+		'textarea'   => array(),
+		'label'      => array(),
+		'button'     => array(),
+		'script'     => array(),
+		'noscript'   => array(),
+		'meta'       => array(),
+		'link'       => array(),
+		'style'      => array(),
+		'abbr'       => array(),
+		'acronym'    => array(),
+		'address'    => array(),
+		'applet'     => array(),
+		'area'       => array(),
+		'article'    => array(),
+		'aside'      => array(),
+		'audio'      => array(),
+		'b'          => array(),
+		'base'       => array(),
+		'bdi'        => array(),
+		'bdo'        => array(),
+		'big'        => array(),
+		'body'       => array(),
+		'canvas'     => array(),
+		'caption'    => array(),
+		'center'     => array(),
+		'cite'       => array(),
+		'col'        => array(),
+		'colgroup'   => array(),
+		'datalist'   => array(),
+		'dd'         => array(),
+		'del'        => array(),
+		'details'    => array(),
+		'dfn'        => array(),
+		'dialog'     => array(),
+	);
+}
+
+/**
+ * Sanitize the PHP code snippets.
+ *
+ * @since 1.0.0
+ * @param string $php_code PHP code snippet.
+ */
+function insertcodes_sanitize_php_snippets( $php_code ) {
+	$allowed_tags = array(
+		'br'     => array(),
+		'em'     => array(),
+		'strong' => array(),
+		'p'      => array(),
+		// Add more allowed HTML tags here.
+	);
+	$allowed_php_tags = array(
+		'php'  => array(),
+		'echo' => array(),
+		'if'   => array(),
+		'else' => array(),
+		// Add more allowed PHP constructs if necessary.
+	);
+
+	// Combine allowed tags.
+	$sanitized_code = wp_kses( $php_code, array_merge( $allowed_tags, $allowed_php_tags ) );
+
+	return $sanitized_code;
+}
